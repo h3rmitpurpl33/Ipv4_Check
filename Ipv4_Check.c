820b1d7eac594f5dfc08df44ab8811794ebbfb7b
h3rmit <76179581+h3rmitpurpl33@users.noreply.github.com>
2021-08-30 00:27:36 +0300

    Update Ipv4_Check.c

--- Ipv4_Check.c
+++ Ipv4_Check.c
@@ -1,47 +1,38 @@
-int main(){
-int ch,dots,bytes,temp;
+int main() {
+    int ch, dots, bytes, temp;
 
-dots = bytes = temp = 0;
-printf("enter ip adress (x.x.x.x) : ");
-while((ch = getchar()) != '\n' && ch != EOF){
-    if(ch < '0' || ch > '9'){
-        if(ch == '.')
-        {
-            dots++;
-            if(temp != -1)
-            {
-                if(temp > 255)
-                {
-                    printf("the value of each byte should be in [0,255]\n");
+    dots = bytes = temp = 0;
+    printf("enter ip adress (x.x.x.x) : ");
+    while ((ch = getchar()) != '\n' && ch != EOF) {
+        if (ch < '0' || ch > '9') {
+            if (ch == '.') {
+                dots++;
+                if (temp != -1) {
+                    if (temp > 255) {
+                        printf("the value of each byte should be in [0,255]\n");
+                    }
+                    bytes++;
+                    temp = -1;
                 }
-                bytes++;
-                temp = -1;
+            } else {
+                printf("error acceptable chars are only digits and dots\n");
             }
-        }else{
-            printf("error acceptable chars are only digits and dots\n");
+        } else {
+            if (temp == -1) {
+                temp = 0;
+            }
+            temp = 10 * temp + (ch - '0');
         }
-    }else
-    {
-        if(temp == -1)
-        {
-            temp = 0;
+    }
+    if (temp != -1) {
+        if (temp > 255) {
+            printf("the value of each byte should be in [0,255]\n");
+            return 0;
         }
-        temp = 10*temp + (ch-'0');
+        bytes++;
     }
-}
-if(temp != -1)
-{
-    if(temp > 255)
-    {
-        printf("the value of each byte should be in [0,255]\n");
-        return 0;
+    if (dots != 3 || bytes != 4) {
+        printf("error : the ip format should be x.x.x.x\n");
+    } else {
+        printf("the input adress is a valid Ipv4 adress\n");
     }
-    bytes++;
-}
-if(dots != 3 || bytes != 4)
-{
-    printf("error : the ip format should be x.x.x.x\n");
-}else
-{
-    printf("the input adress is a valid Ipv4 adress\n");
-}
